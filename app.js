3aa6f1a6840f33c425d9d429d97fb503c6511922
Cole DeVore <c16devore@gmail.com>
2022-07-12 10:25:13 -0700

    fixed thhe issues with the 'printEmployeeForm' function it now takes the overridden status and is able to be logged corrcetly

--- app.js
+++ app.js
@@ -5,22 +5,20 @@ this.name = name
 this.jobTitle = jobTitle
 this.salary = salary
 this.status = status 
- 
-function printEmployeeForm () {
-
-    console.log("Name: "+name+",", "Job Title :"+jobTitle +",","Salary: "+ salary+ ",","Status: " +status)
+    this.printEmployeeForm = function() {
+        console.log("Name: "+this.name+",", "Job Title :"+this.jobTitle +",","Salary: "+ this.salary+ ",","Status: " +this.status)
     }
-printEmployeeForm()
 }
 let employee1 = new Employee("Jim", "Host", "10k/year", )
 employee1.status = "Contract"
-// ask about the printEmployeeForm problem where the status overide wasnt working 
+
 let employee2 = new Employee("Alyssa", "Manager", "30k/year" )
 let employee3 = new Employee("Craig", "Dish Washer", "15k/year")
 
 employees.push(employee1,employee2,employee3)
 console.log(employees)
-// employee1.printEmployeeForm()
+
+employee1.printEmployeeForm()
 // console.log(employee1,employee2,employee3)
 
 // console.log(`This is an   example.  ${employee1.name}
